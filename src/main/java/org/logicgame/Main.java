ec98d88871be23b5f9f3f3db684f796d8b5d3e7f
Dzida <dominik.dzida@o2.pl>
2024-10-17 21:47:49 +0200

    java init
    Setting up java

---
+++ src/main/java/org/logicgame/Main.java
@@ -0,0 +1,29 @@
+package org.logicgame;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button("Click me!");
+        btn.setOnAction(event -> System.out.println("Button clicked!"));
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+
+        Scene scene = new Scene(root, 400, 300);
+
+        primaryStage.setTitle("Logic Gates Game");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
