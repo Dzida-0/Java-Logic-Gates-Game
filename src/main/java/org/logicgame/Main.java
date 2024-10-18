444e53f2f1ba700a59a83d052399f4052c2c277a
Dzida <dominik.dzida@o2.pl>
2024-10-18 22:07:25 +0200

    Window UI and controls
    Implementation of basic window UI and some control buttons

--- src/main/java/org/logicgame/Main.java
+++ src/main/java/org/logicgame/Main.java
@@ -5,22 +5,14 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import org.logicgame.ui.MainGameWindow;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button("Click me!");
-        btn.setOnAction(event -> System.out.println("Button clicked!"));
+     new MainGameWindow(primaryStage,"name");
 
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-
-        Scene scene = new Scene(root, 400, 300);
-
-        primaryStage.setTitle("Logic Gates Game");
-        primaryStage.setScene(scene);
-        primaryStage.show();
     }
 
     public static void main(String[] args) {
---
+++ src/main/java/org/logicgame/logic/ProgramInput.java
@@ -0,0 +1,4 @@
+package org.logicgame.logic;
+
+public class ProgramInput {
+}
