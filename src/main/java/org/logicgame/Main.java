98e289156f74356f8a2a03694cee95e4869e17eb
Dzida <dominik.dzida@o2.pl>
2024-10-18 23:26:53 +0200

    Logic gates, connectors and circuit
    Implementing or, and, not gates. Adding circuit elements

--- src/main/java/org/logicgame/Main.java
+++ src/main/java/org/logicgame/Main.java
@@ -5,6 +5,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import org.logicgame.logic.Circuit;
 
 public class Main extends Application {
 
@@ -12,7 +13,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         Button btn = new Button("Click me!");
         btn.setOnAction(event -> System.out.println("Button clicked!"));
-
+        Circuit c = new Circuit(3,3);
         StackPane root = new StackPane();
         root.getChildren().add(btn);
 
---
+++ src/main/java/org/logicgame/logic/ProgramOutput.java
@@ -0,0 +1,26 @@
+package org.logicgame.logic;
+
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.input.MouseEvent;
+
+public class ProgramOutput extends Circle {
+    private boolean curentState = false;
+    private int id;
+    public ProgramOutput(int id) {
+        this.id = id;
+        setRadius(20);
+        setFill(Color.GRAY);
+        setStroke(Color.BLACK);
+        setStrokeWidth(3);
+        setOnMouseClicked(this::handleClick);
+    }
+
+    private void handleClick(MouseEvent event) {
+        curentState = !curentState;
+        if (curentState) {setFill(Color.WHITE); }
+        else {setFill(Color.GRAY);}
+    }
+
+}
